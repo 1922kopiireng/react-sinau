cbd9adb2bb392d62f850b0e33772e860ef5c6f8e
1922kopiireng <kopiireng1922@gmail.com>
2022-08-30 13:56:38 +0700

    React js to do list

---
+++ src/todo-list/TodoList.js
@@ -0,0 +1,14 @@
+import './TodoList.css'
+
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn react</li>
+            <li>Build Tool</li>
+            <li>Record</li>
+        </ul>
+    )
+}
+
+export default TodoList
